fa7f438112082017f7b6d0dc2d9ad209c16e8948
bloodmas <bloodmas78@gmail.com>
2026-07-07 00:01:39 +0900

    fix: remove unused useEffect import in ThreeLogo.tsx

--- src/components/ThreeLogo.tsx
+++ src/components/ThreeLogo.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useEffect } from 'react'
+import { useMemo, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Environment } from '@react-three/drei'
 import * as THREE from 'three'
